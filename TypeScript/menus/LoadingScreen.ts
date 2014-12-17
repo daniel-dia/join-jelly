d52877584ad99f5e3bc62aaf8a2a6c50c308433a
Daniel Santos <artsjedi@hotmail.com>
2014-12-16 22:08:46 -0200

    uncomment loading for sounds

--- TypeScript/menus/LoadingScreen.ts
+++ TypeScript/menus/LoadingScreen.ts
@@ -18,8 +18,7 @@ module joinjelly {
             assetscale = 1;
             if (window.innerWidth <= 1024) assetscale = 0.5;
             if (window.innerWidth <= 384) assetscale = 0.25;
-            assetscale = 1;
-
+         
             var queue = gameui.AssetsManager.loadAssets(this.getAssetsManifest(assetscale));//, spriteSheets, images);
 
             //var loader = Assets.loadAssets();
@@ -137,33 +136,33 @@ module joinjelly {
 
                 { id: "pediaItem", src: "assets/images_" + scale + "x/pediaItem.png" },
                 { id: "BigBG", src: "assets/images_" + scale + "x/BigBG.png" }, 
-
-                //{ id: "bg1", src: "Sounds/bg1.mp3" },
-
-
-                //{ id: "sound_h1", src: "Sounds/h1.mp3" },
-                //{ id: "sound_r1", src: "Sounds/r1.mp3" },
-
-                //{ id: "sound_s1", src: "Sounds/s1.mp3" },
-                //{ id: "sound_s2", src: "Sounds/s2.mp3" },
-                //{ id: "sound_s3", src: "Sounds/s3.mp3" },
-
-                //{ id: "sound_j1", src: "Sounds/j1.mp3" },
-                //{ id: "sound_j2", src: "Sounds/j2.mp3" },
-                //{ id: "sound_j3", src: "Sounds/j3.mp3" },
-                //{ id: "sound_j4", src: "Sounds/j4.mp3" },
-
-                //{ id: "Interface Sound-06", src: "Sounds/Interface Sound-06.mp3" },
-                //{ id: "Interface Sound-07", src: "Sounds/Interface Sound-07.mp3" },
-                //{ id: "Interface Sound-08", src: "Sounds/Interface Sound-08.mp3" },
-                //{ id: "Interface Sound-09", src: "Sounds/Interface Sound-09.mp3" },
-                //{ id: "Interface Sound-11", src: "Sounds/Interface Sound-11.mp3" },
-                //{ id: "Interface Sound-14", src: "Sounds/Interface Sound-14.mp3" },
-                //{ id: "Interface Sound-15", src: "Sounds/Interface Sound-15.mp3" },
-                //{ id: "end", src: "Sounds/end.mp3" },
-
-                //{ id: "musicIntro", src: "Sounds/musicIntro.mp3" },
-                //{ id: "music1", src: "Sounds/music1.mp3" },
+                
+                { id: "bg1", src: "Sounds/bg1.mp3" },
+                
+                
+                { id: "sound_h1", src: "Sounds/h1.mp3" },
+                { id: "sound_r1", src: "Sounds/r1.mp3" },
+                
+                { id: "sound_s1", src: "Sounds/s1.mp3" },
+                { id: "sound_s2", src: "Sounds/s2.mp3" },
+                { id: "sound_s3", src: "Sounds/s3.mp3" },
+                
+                { id: "sound_j1", src: "Sounds/j1.mp3" },
+                { id: "sound_j2", src: "Sounds/j2.mp3" },
+                { id: "sound_j3", src: "Sounds/j3.mp3" },
+                { id: "sound_j4", src: "Sounds/j4.mp3" },
+                
+                { id: "Interface Sound-06", src: "Sounds/Interface Sound-06.mp3" },
+                { id: "Interface Sound-07", src: "Sounds/Interface Sound-07.mp3" },
+                { id: "Interface Sound-08", src: "Sounds/Interface Sound-08.mp3" },
+                { id: "Interface Sound-09", src: "Sounds/Interface Sound-09.mp3" },
+                { id: "Interface Sound-11", src: "Sounds/Interface Sound-11.mp3" },
+                { id: "Interface Sound-14", src: "Sounds/Interface Sound-14.mp3" },
+                { id: "Interface Sound-15", src: "Sounds/Interface Sound-15.mp3" },
+                { id: "end", src: "Sounds/end.mp3" },
+                
+                { id: "musicIntro", src: "Sounds/musicIntro.mp3" },
+                { id: "music1", src: "Sounds/music1.mp3" },
 
             ]
         }
