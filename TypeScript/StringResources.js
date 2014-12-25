e1fd0d699a4d51b599238397e424b51bce909b26
Daniel Santos <artsjedi@hotmail.com>
2014-12-25 16:39:03 -0200

    String Resources

---
+++ TypeScript/StringResources.js
@@ -0,0 +1,41 @@
+var StringResources = {
+    menus: {
+        highScore: "High Score",
+        loading: "loading",
+        score: "score",
+        level: "level",
+        options: "options",
+        highJelly: "High Jelly",
+        gameOver: "GAME OVER",
+        jellypedia: "Jellypedia",
+        pause: "paused",
+        sound: "Sound"
+    },
+    tutorial: {
+        msg1: "Hello, \nI'm little Jelly",
+        msg2: "Help me to evolve\nJoin another jelly to me",
+        msg3: "Great! now I'm bigger, \nevolve me once more",
+        msg4: "Ow, a dirt appears",
+        msg5: "Join two jelly near to destroy it.",
+        msg6: "Perfect!\nNow let's play this game.",
+        msg7: "but be careful, \ndo not let the board fill, \nthis is the end for us.",
+    },
+    jellys: {
+        1: { name: "Little Jelly", description: "description" },
+        2: { name: "Go Jelly", description: "description" },
+        4: { name: "Angry Jelly", description: "description" },
+        8: { name: "Kawaii Jelly", description: "description" },
+        16: { name: "indiferent Jelly", description: "description" },
+        32: { name: "Bored Jelly", description: "description" },
+        64: { name: "Sad Jelly", description: "description" },
+        128: { name: "Histeric Jelly", description: "description" },
+        256: { name: "Crazy Jelly", description: "description" },
+        512: { name: "Phanton1 Jelly", description: "description" },
+        1024: { name: "Phanton2 Jelly", description: "description" },
+        2048: { name: "Phanton3 Jelly", description: "description" },
+        4096: { name: "Phanton4 Jelly", description: "description" },
+        8192: { name: "Phanton5 Jelly", description: "description" },
+    },
+    credit: {}
+};
+//# sourceMappingURL=StringResources.js.map
