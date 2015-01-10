5d79c13e23b0e6dad7df617507cd49f312577086
Daniel Santos <artsjedi@hotmail.com>
2015-01-10 11:46:44 -0200

    Change product prices

--- TypeScript/InAppPurchases.js
+++ TypeScript/InAppPurchases.js
@@ -5,12 +5,12 @@ var InAppPurchases = (function () {
         setTimeout(function () {
             if (callback)
                 callback([
-                    { ProductId: "time3x", Name: "3x Time", Description: "Removes all little\njellys and dirtys", FormattedPrice: "R$1,99", },
-                    { ProductId: "fast3x", Name: "3x Fast", Description: "Removes all little\njellys and dirtys", FormattedPrice: "R$1,99", },
-                    { ProductId: "revive3x", Name: "3x Revive", Description: "Removes all little\njellys and dirtys", FormattedPrice: "R$1,99", },
-                    { ProductId: "clean3x", Name: "3x Clean", Description: "Removes all little\njellys and dirtys", FormattedPrice: "R$1,99", },
-                    { ProductId: "pack3x", Name: "Item Pack 3x", Description: "Removes all little\njellys and dirtys", FormattedPrice: "R$4,99", },
-                    { ProductId: "pack9x", Name: "Item Pack 9x", Description: "Removes all little\njellys and dirtys", FormattedPrice: "R$8,99", },
+                    { ProductId: "time5x", Name: "5x Time", Description: "Removes all little\njellys and dirtys", FormattedPrice: "R$1,99", },
+                    { ProductId: "fast5x", Name: "5x Fast", Description: "Removes all little\njellys and dirtys", FormattedPrice: "R$1,99", },
+                    { ProductId: "revive5x", Name: "5x Revive", Description: "Removes all little\njellys and dirtys", FormattedPrice: "R$1,99", },
+                    { ProductId: "clean5x", Name: "5x Clean", Description: "Removes all little\njellys and dirtys", FormattedPrice: "R$1,99", },
+                    { ProductId: "pack5x", Name: "Item Pack 3x", Description: "Removes all little\njellys and dirtys", FormattedPrice: "R$4,99", },
+                    { ProductId: "pack10x", Name: "Item Pack 9x", Description: "Removes all little\njellys and dirtys", FormattedPrice: "R$6,99", },
                     { ProductId: "lucky", Name: "Lucky Clover", Description: "Removes all little\njellys and dirtys", FormattedPrice: "R$3,99", },
                 ]);
         }, 1000);
