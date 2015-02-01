e16704a99b6f5d3609abfb6057fee87b6eb4b98e
Daniel Santos <artsjedi@hotmail.com>
2015-02-01 18:40:07 -0200

    reference TS for compilation

---
+++ _references.ts
@@ -0,0 +1,57 @@
+﻿/// <reference path="scripts/gameui/uiitem.ts" />
+ 
+
+/// <reference path="scripts/gameui/AudioManager.ts" />
+/// <reference path="scripts/gameui/ImagesManager.ts" />
+/// <reference path="scripts/gameui/GameScreen.ts" />
+/// <reference path="scripts/gameui/UIItem.ts" />
+/// <reference path="scripts/gameui/Grid.ts" />
+/// <reference path="scripts/gameui/Label.ts" />
+/// <reference path="scripts/gameui/MenuContainer.ts" />
+/// <reference path="scripts/gameui/ScreenState.ts" />
+/// <reference path="scripts/gameui/Transition.ts" />
+/// <reference path="scripts/gameui/Button.ts" />
+
+/// <reference path="scripts/src/ItemsData.ts" />
+/// <reference path="scripts/src/StringResources.ts" />
+/// <reference path="scripts/src/Definitions.ts" />
+/// <reference path="scripts/src/Analytics.ts" />
+/// <reference path="scripts/src/InAppPurchases.ts" />
+
+/// <reference path="scripts/src/menus/ScrollabePage.ts" />
+/// <reference path="scripts/src/menus/LoadingScreen.ts" />
+/// <reference path="scripts/src/menus/MainScreen.ts" />
+/// <reference path="scripts/src/menus/Jellypedia.ts" />
+/// <reference path="scripts/src/menus/view/Title.ts" />
+/// <reference path="scripts/src/menus/view/JellyLobby.ts" />
+/// <reference path="scripts/src/menus/view/FlyOutMenu.ts" />
+/// <reference path="scripts/src/menus/view/JellypediaItem.ts" />
+/// <reference path="scripts/src/menus/view/StoreItem.ts" />
+/// <reference path="scripts/src/menus/StoreMenu.ts" />
+/// <reference path="scripts/src/menus/MainMenu.ts" />
+/// <reference path="scripts/src/menus/Credit.ts" />
+/// <reference path="scripts/src/menus/view/SoundOptions.ts" />
+
+/// <reference path="scripts/src/view/Jellyble.ts" />
+/// <reference path="scripts/src/view/LoadingBall.ts" />
+
+/// <reference path="scripts/src/gameplay/view/GameHeader.ts" />
+/// <reference path="scripts/src/gameplay/view/GameMenu.ts" />
+/// <reference path="scripts/src/gameplay/view/Jelly.ts" />
+/// <reference path="scripts/src/gameplay/view/LevelIndicator.ts" />
+/// <reference path="scripts/src/gameplay/view/ItemButton.ts" />
+/// <reference path="scripts/src/gameplay/view/GameFooter.ts" />
+/// <reference path="scripts/src/gameplay/view/PauseMenu.ts" />
+/// <reference path="scripts/src/gameplay/view/TimeBar.ts" />
+/// <reference path="scripts/src/gameplay/view/FinishDialog.ts" />
+/// <reference path="scripts/src/gameplay/view/TutorialMove.ts" />
+/// <reference path="scripts/src/gameplay/view/TutorialMessage.ts" />
+/// <reference path="scripts/src/gameplay/Board.ts" />
+/// <reference path="scripts/src/gameplay/Tile.ts" />
+/// <reference path="scripts/src/gameplay/GamePlay.ts" />
+/// <reference path="scripts/src/gameplay/ExplodeBrick.ts" />
+/// <reference path="scripts/src/gameplay/Tutorial.ts" />
+
+/// <reference path="scripts/src/Message.ts" />
+/// <reference path="scripts/src/UserData.ts" />
+/// <reference path="scripts/src/JoinJelly.ts" />
