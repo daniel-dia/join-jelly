6568d9334168745b3a102613e2601f93977777c5
Daniel Santos <artsjedi@hotmail.com>
2015-02-05 19:55:08 -0200

    Audio Manager

--- gameui/AudioManager.ts
+++ gameui/AudioManager.ts
@@ -1,30 +1,37 @@
 ﻿module gameui {
 
     // Class
-    export class AudiosManager{
+    export class AudiosManager {
 
-       private static loader: createjs.LoadQueue;
-   
+        private static loader: createjs.LoadQueue;
+
         private static currentMusicName: string;
         private static currentMusic: createjs.SoundInstance
         private static musicVolue: number;
         private static soundVolume: number;
-        
+
         public static setMusicVolume(volume: number) {
             if (this.currentMusic) this.currentMusic.volume = volume;
-            this.musicVolue = volume; 
+            this.musicVolue = volume;
         }
 
-        public static setSoundVeolume(volume: number) { 
+        public static setSoundVeolume(volume: number) {
+
             this.soundVolume = volume;
         }
 
-        public static getMusicVolume(): number { return this.musicVolue; }
+        public static getMusicVolume(): number {
+            if (this.musicVolue == undefined) return 1;
+            return this.musicVolue;
+        }
 
-        public static getSoundVolume(): number { return this.soundVolume; } 
+        public static getSoundVolume(): number {
+            if (this.soundVolume == undefined) return 1;
+            return this.soundVolume;
+        }
 
-        public static playMusic(name: string,volume:number=1) {
-              if (this.currentMusic) {
+        public static playMusic(name: string, volume: number= 1) {
+            if (this.currentMusic) {
                 this.currentMusic.setVolume(volume);
                 if (this.currentMusicName == name) return;
 
@@ -33,13 +40,13 @@
             }
 
             this.currentMusicName = name;
-            this.currentMusic = createjs.Sound.play(name,null,null,null,1000);
+            this.currentMusic = createjs.Sound.play(name, null, null, null, 1000);
             this.currentMusic.setVolume(volume * this.getMusicVolume());
         }
 
         public static playSound(name: string, interrupt?: boolean, delay: number= 0, offset?: number, loop?: number, volume: number= 1): createjs.SoundInstance {
-            
-            return createjs.Sound.play(name, interrupt, delay, offset, loop, volume*this.getSoundVolume());
+
+            return createjs.Sound.play(name, interrupt, delay, offset, loop, volume * this.getSoundVolume());
         }
 
         // #endregion
