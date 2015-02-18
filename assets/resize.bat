698af96ec1e3ec80ab6b2e507a15d598d520a5c6
Daniel Santos <artsjedi@hotmail.com>
2015-02-18 21:51:07 -0200

    ASsets

--- assets/resize.bat
+++ assets/resize.bat
@@ -1,10 +1,17 @@
-xcopy images images_0.5x /y /s
-cd images_0.5x
-for /r %%f in (*.*) do convert %%f -resize 50%% %%f
-cd..
-xcopy images_0.5x images_0.25x /y /s
-cd images_0.25x
-for /r %%f in (*.*) do convert %%f -resize 50%% %%f
-cd..
+@echo off
+call:copyAndResize images images_0.5x 50%%%% 
+call:copyAndResize images images_0.25x 25%%%% 
+goto:eof
 
+:copyAndResize
+echo. Resising "%~1"  "%~2"  at "%~3"
+for /f %%a in ('xcopy "%~1"  "%~2" /L /D /Y') do @(
+	if exist "%%a" (
+		echo. %%a 
+		xcopy "%%a" "%~2" /D /Y /Q > nul
+		convert %~2/%%~nxa -resize %~3 %~2/%%~nxa 
+	)
+)
+
+goto:eof
 @pause
